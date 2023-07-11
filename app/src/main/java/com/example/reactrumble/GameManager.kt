18116a411cccb051bed1a4bf9cdb2ad4d6553236
brufatto <lg260638@dal.ca>
2023-07-11 15:59:23 -0300

    Added basic engine and scorekeeping functionality. Engine can swap between multiple activities randomly and keep score

---
+++ app/src/main/java/com/example/reactrumble/GameManager.kt
@@ -0,0 +1,69 @@
+package com.example.reactrumble
+
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat.startActivity
+import kotlin.random.Random
+
+class GameManager private constructor(var list : Array<Class <out AppCompatActivity>>, var maxScore : Int) {
+
+    companion object {
+
+        var instance : GameManager? = null
+
+        var playerOneScore : Int
+        var playerTwoScore : Int
+        var maxScore: Int = 0
+        var gameStarted = false
+        private lateinit var list : Array<Class <out AppCompatActivity>>
+
+        init {
+            playerOneScore = 0
+            playerTwoScore = 0
+        }
+
+        fun initialize(list: Array<Class<out AppCompatActivity>>, maxScore: Int) {
+            if(instance != null) {
+                return
+            }
+            instance = GameManager(list, maxScore)
+            this.maxScore = maxScore
+            this.list = list
+        }
+
+        fun startGame(context: Context) {
+            if(!gameStarted) {
+                nextGame(context)
+                gameStarted = true
+            }
+        }
+
+        /**
+         * If game is not over, we swtich to a random game.
+         */
+        fun nextGame(context: Context) {
+            if (playerOneScore >= maxScore || playerTwoScore >= maxScore) {
+                gameOver(context)
+            } else {
+                val index = Random.nextInt(list.size)
+                val intent = Intent(context, list[index])
+                context.startActivity(intent)
+            }
+
+        }
+
+        /**
+         * If gameOver is called, we move to the gameOver screen.
+         */
+        fun gameOver(context: Context) {
+            val intent = Intent(context, GameOverActivity::class.java)
+            context.startActivity(intent)
+            instance = null
+        }
+
+    }
+
+
+}
--- app/src/main/java/com/example/reactrumble/MainActivity.kt
+++ app/src/main/java/com/example/reactrumble/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.reactrumble
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
+		val activityArr = arrayOf(TestGameActivity1::class.java, TestGameActivity2::class.java)
+		GameManager.initialize(activityArr, 5)
+
+		val button: Button = findViewById(R.id.startButton)
+		button.setOnClickListener{
+			GameManager.startGame(this)
+		}
 	}
+
 }
