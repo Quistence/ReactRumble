6a162e314f62c4d2fa93b03819ed250a91c2841a
Elyas Imran Moh Al Belushi <el992401@dal.ca>
2023-07-25 12:38:38 -0300

    Added home screen animation

--- app/src/main/java/com/example/reactrumble/HomeActivity.kt
+++ app/src/main/java/com/example/reactrumble/HomeActivity.kt
@@ -1,10 +1,12 @@
 package com.example.reactrumble
 
+import android.animation.ValueAnimator
 import android.content.Intent
 import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 
 class HomeActivity : AppCompatActivity() {
 	private val PREFS_FILENAME = "customizationsPreferences"
@@ -44,6 +46,12 @@ class HomeActivity : AppCompatActivity() {
 		val startGame: Button = findViewById(R.id.quickStartButton)
 		val games = arrayOf(MathGame::class.java, FlagsGame::class.java, ColorsGame::class.java)
 
+		// Find the React Rumble icon
+		val iconImage: ImageView = findViewById(R.id.provinceImage)
+
+		// Start the animation
+		startBounceAnimation(iconImage)
+
 		startGame.setOnClickListener {
 			//startActivity(Intent(this@HomeActivity, MathGame::class.java))
 			GameManager.startGame(this, games)
@@ -56,5 +64,22 @@ class HomeActivity : AppCompatActivity() {
 		customizeButton.setOnClickListener{
 			startActivity(Intent(this@HomeActivity, CustomizeActivity::class.java))
 		}
+	}
+
+	private fun startBounceAnimation(iconImage: ImageView) {
+		// Set up the animator to scale the ImageView
+		val animator = ValueAnimator.ofFloat(1f, 1.5f, 1f)
+		animator.duration = 2000 // Duration of the animation in milliseconds
+		animator.addUpdateListener { animation ->
+			val scale = animation.animatedValue as Float
+			iconImage.scaleX = scale
+			iconImage.scaleY = scale
+		}
+
+		// Set the repeat count to INFINITE for continuous animation
+		animator.repeatCount = ValueAnimator.INFINITE
+
+		// Start the animator
+		animator.start()
 	}
 }
